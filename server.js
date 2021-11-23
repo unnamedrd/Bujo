4ec8872b22c13b99b205ca9f25222f73bf4f2ead
miguelkristian17 <Miguelkmartinez@gmail.com>
2021-11-23 11:50:52 -0800

    Routing and loginreg

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
-
+app.use(express.static(__dirname + '/views'));
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 //Connect Database
 connectDB();
 
 //Init Middleware
 app.use(express.json({extended: false}))
 
-//Define Routes
+//Define API Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
+
+//Server static assets
+app.get("/login", (req, res) => {
+    res.render('login');
+});
+
+app.get("/register", (req, res) => {
+    res.render('register');
+});
+
+app.get("/journal", (req, res) => {
+    res.render('journal');
+});
+
 
 const PORT = process.env.PORT || 5000;
 
